63b29ee0646502366ad7896c46ef1c1e89da4b1d
Raffaele <raffaele.mentina@gmail.com>
2023-02-13 20:51:13 +0100

    Parte di commit precedente

--- src/main/java/com/psicodramma/model/Edizione.java
+++ src/main/java/com/psicodramma/model/Edizione.java
@@ -9,4 +9,53 @@ public class Edizione extends Interagibile{
     private String lingua;
     private String paese;
     private Opera opera;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getDataPubblicazione() {
+        return dataPubblicazione;
+    }
+    public void setDataPubblicazione(int dataPubblicazione) {
+        this.dataPubblicazione = dataPubblicazione;
+    }
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public String getEditore() {
+        return editore;
+    }
+    public void setEditore(String editore) {
+        this.editore = editore;
+    }
+    public String getLingua() {
+        return lingua;
+    }
+    public void setLingua(String lingua) {
+        this.lingua = lingua;
+    }
+    public String getPaese() {
+        return paese;
+    }
+    public void setPaese(String paese) {
+        this.paese = paese;
+    }
+    public Opera getOpera() {
+        return opera;
+    }
+    public void setOpera(Opera opera) {
+        this.opera = opera;
+    }    
 }
--- src/main/java/com/psicodramma/model/Interagibile.java
+++ src/main/java/com/psicodramma/model/Interagibile.java
@@ -5,5 +5,17 @@ import java.util.Set;
 public abstract class Interagibile {
     private Set<Utente> like;
     private Set<Commento> commenti;
-    
+
+    public Set<Utente> getLike() {
+        return like;
+    }
+    public void setLike(Set<Utente> like) {
+        this.like = like;
+    }
+    public Set<Commento> getCommenti() {
+        return commenti;
+    }
+    public void setCommenti(Set<Commento> commenti) {
+        this.commenti = commenti;
+    }
 }
