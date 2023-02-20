e8148e7f4559738229e05946f3089df2da0eced9
Raffaele <raffaele.mentina@gmail.com>
2023-02-20 15:38:52 +0100

    Robe Libro e grafico azioni

--- src/main/java/com/psicodramma/controller/TimelineController.java
+++ src/main/java/com/psicodramma/controller/TimelineController.java
@@ -1,9 +1,8 @@
 package com.psicodramma.controller;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import com.psicodramma.App;
 import com.psicodramma.UIControl.ActionPane;
@@ -11,18 +10,30 @@ import com.psicodramma.model.Azione;
 import com.psicodramma.model.Utente;
 import com.psicodramma.service.TimelineService;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.ListView;
+
 public class TimelineController {
     @FXML protected ListView<Azione> actionViewList;
-
+    @FXML protected BarChart<String, Long> actionWeekChart;
     private ObservableList<Azione> actionList = FXCollections.observableArrayList();
     private TimelineService timelineService;
     private Utente utente;
+    private Map<String, Long> azioniGiorno;
     
     public TimelineController() {
         this.utente = ((Utente)App.getData());
         timelineService = new TimelineService();
-        var res = timelineService.getActionList(new Utente());
+        var res = timelineService.getActionList();
         actionList.setAll(res);
+        azioniGiorno = timelineService.getAzioniGiorno(utente);
+        
     }
      
     @FXML
@@ -32,6 +43,7 @@ public class TimelineController {
         } else {
             System.out.println("action list is empty");
         }
+        setupActionWeek();
     }
 
     private void setupListView() {
@@ -39,5 +51,17 @@ public class TimelineController {
         actionViewList.setCellFactory((param) -> new ActionPane());
     }
 
+    private void setupActionWeek(){
+        Stream<LocalDate> giorni = LocalDate.now().minusDays(6).datesUntil(LocalDate.now().plusDays(1));
+        XYChart.Series<String, Long> serie = new Series<>();
+        giorni.forEach(x ->{
+            if(azioniGiorno.containsKey(x.toString())){
+                serie.getData().add(new Data<>(x.toString(), azioniGiorno.get(x.toString())));
+            }else{
+                serie.getData().add(new Data<String,Long>(x.toString(), Long.parseLong("0")));
+            }
+        });
+        actionWeekChart.getData().add(serie);
+    }
 
 }
--- src/main/java/com/psicodramma/service/TimelineService.java
+++ src/main/java/com/psicodramma/service/TimelineService.java
@@ -1,25 +1,29 @@
 package com.psicodramma.service;
 
 import java.util.List;
+import java.util.Map;
 
+import com.psicodramma.dao.AzioneDao;
 import com.psicodramma.model.Azione;
 import com.psicodramma.model.Utente;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.NoResultException;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.PersistenceException;
 
 public class TimelineService {
 
     private EntityManagerFactory emf = Persistence.createEntityManagerFactory("default"); 
 
-    public List<Azione> getActionList(Utente u){
+    public List<Azione> getActionList(){
         List<Azione> res = null; 
         EntityManager em = emf.createEntityManager();
         res = em.createNativeQuery("select a.* from azione a", Azione.class).getResultList();
-
         return res;
+    }
+
+    public Map<String, Long> getAzioniGiorno(Utente u){
+        AzioneDao ad = new AzioneDao();
+        return ad.getAzioniGiorno(u.getUsername());
     }
 }
