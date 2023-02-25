aa6a762b89b2fb2ca678522d46ee1031b50a4885
silviu48 <silviu48vatamanelu@gmail.com>
2023-02-25 20:06:02 +0100

    MOdifica Stato interfacci

--- src/main/java/com/psicodramma/UIControl/DialogAggiungi.java
+++ src/main/java/com/psicodramma/UIControl/DialogAggiungi.java
@@ -2,23 +2,32 @@ package com.psicodramma.UIControl;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Optional;
 
 import com.psicodramma.model.Raccolta;
 import com.psicodramma.model.Utente;
 import com.psicodramma.service.LibraryService;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
+import javafx.util.Pair;
 
 public class DialogAggiungi extends Dialog<Raccolta>{
     @FXML private DialogPane dPane;
@@ -71,13 +80,48 @@ public class DialogAggiungi extends Dialog<Raccolta>{
 
     @FXML
     private void newRaccolta(){
-        TextInputDialog alert = new TextInputDialog();
-        alert.setHeaderText("Inserisci il nome della raccolta che vuoi creare");
-        alert.showAndWait();
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Nuova raccolta");
+        dialog.setHeaderText("Inserisci i dati per creare una nuova raccolta");
 
-        if (alert.getResult() != null && alert.getResult().trim() != "") {
-            obsList.add(libraryService.createRaccolta(alert.getResult().trim(), "", utente));
-        }
+        ButtonType buttonConferma = new ButtonType("Conferma", ButtonData.OK_DONE);
+        ButtonType buttonCancella = new ButtonType("Cancella", ButtonData.CANCEL_CLOSE);
+        dialog.getDialogPane().getButtonTypes().addAll(buttonCancella, buttonConferma);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+        
+        TextField nome = new TextField();
+        TextField descrizione = new TextField();
+        
+        grid.add(new Label("Nome:"), 0, 0);
+        grid.add(nome, 1, 0);
+        grid.add(new Label("Descrizione:"), 0, 1);
+        grid.add(descrizione, 1, 1);
+
+        // Validazione
+        Node loginButton = dialog.getDialogPane().lookupButton(buttonConferma);
+        nome.textProperty().addListener((observable, oldValue, newValue) -> {
+            loginButton.setDisable(newValue.trim().isEmpty());
+        });
+
+        dialog.getDialogPane().setContent(grid);
+        Platform.runLater(() -> nome.requestFocus());
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == buttonConfirm) {
+                return new Pair<>(nome.getText().trim(), descrizione.getText().trim());
+            }
+            return null;
+        });
+
+        Optional<Pair<String, String>> result = dialog.showAndWait();
+
+        result.ifPresent(usernamePassword -> {
+            System.out.println("Username=" + usernamePassword.getKey() + ", Password=" + usernamePassword.getValue());
+        });
     }
 
     @FXML
